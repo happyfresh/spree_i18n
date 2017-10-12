ce77e7f1bdaff794a66cc9e3bfc72280adee3139
Fajar Budiprasetyo <fajar.ab@gmail.com>
2017-10-13 06:02:05 +0700

    Do not load frontend gems unless Spree::Backend is defined

--- lib/spree_i18n.rb
+++ lib/spree_i18n.rb
@@ -1,6 +1,10 @@
 require 'rails-i18n'
-require 'sass/rails'
 require 'spree_core'
 require 'spree_i18n/engine'
 require 'spree/i18n_utils'
-require 'coffee_script'
+
+
+if defined? Spree::Backend
+  require 'sass/rails'
+  require 'coffee_script'
+end
