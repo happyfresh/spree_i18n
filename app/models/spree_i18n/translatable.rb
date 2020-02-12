a26845df71af7930273316baddee9c53f4d78801
Robby Cahyadi Hendro Saputro <robby.saputro@happyfresh.com>
2020-02-12 17:26:20 +0700

    Fix ransack filtering on translated table

--- app/models/spree_i18n/translatable.rb
+++ app/models/spree_i18n/translatable.rb
@@ -1,20 +1,28 @@
-require 'spree_i18n/ransack_translator'
-
 module SpreeI18n
   module Translatable
     extend ActiveSupport::Concern
 
     included do |klass|
       accepts_nested_attributes_for :translations
-      # klass.whitelisted_ransackable_associations |= ['translations']
     end
 
     class_methods do
       def ransack(params = {}, options = {})
-        translated_params = RansackTranslator.new(self, params).translated_params
+        params ||= {}
+        names = params.keys
 
-        super(translated_params, options)
+        names.each do |n|
+          translated_attribute_names.each do |t|
+            if n.to_s.starts_with? t.to_s
+              params[:"translations_#{n}"] = params[n]
+              params.delete n
+            end
+          end
+        end
+
+        super(params, options)
       end
+      alias :search :ransack unless respond_to? :search
 
       alias_method :search, :ransack unless respond_to? :search
     end
